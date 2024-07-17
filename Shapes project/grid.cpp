cabcb2c0bf83f632c9c0090f7b0901c67005d104
alex-115 <109692636+alex-115@users.noreply.github.com>
2024-07-16 17:49:15 -0700

    Add files via upload

---
+++ Shapes project/grid.cpp
@@ -0,0 +1,42 @@
+// filename: Grid.cpp
+// your name (first and last): Alejandro Barajas
+// your Chico Portal/INGInious username: Ajbarajas1
+// Short summary of the file contents: This executes the constructor, void set function, and print function.
+#include "grid.h"
+#include <iostream>
+using namespace std;
+
+Grid::Grid()
+{
+    const int num_c = 60, num_r = 24;
+    for (int i = 0; i < num_r; i++)
+    {
+            for(int j = 0; j < num_c;j++)
+            {
+            grid[i][j] = ' ';
+            }
+    }
+
+}
+void Grid::set(int x, int y, char c)
+{  
+        if (x >= 0 && x < 60 && y >= 0 && y < 24)
+        {
+        grid[y][x] = c;
+        }
+       
+}
+void Grid::print()
+{
+        int num_r = 24;
+        int num_c = 60;
+       for (int i = 0; i < num_r; i++)
+    {
+            for(int j = 0; j < num_c;j++)
+            {
+             cout << grid[i][j];
+            }
+            cout << endl;
+    }
+}
+
---
+++ Shapes project/shape.cpp
@@ -0,0 +1,12 @@
+// filename: Shape.cpp
+// your name (first and last): Alejandro Barajas
+// your Chico Portal/INGInious username: Ajbarajas1
+// Short summary of the file contents: This contains the constructor of shape that initializes m_x and m_y
+#include "shape.h"
+#include <iostream>
+using namespace std;
+Shape::Shape(int x,int y)
+{
+        m_x = x;
+        m_y = y;
+}
