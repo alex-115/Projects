d9c4cc88bed4f38b271bef36c92f36b3bd88df9f
alex-115 <109692636+alex-115@users.noreply.github.com>
2024-12-31 16:46:54 -0800

    Update shape.cpp

--- Shapes project/shape.cpp
+++ Shapes project/shape.cpp
@@ -1,6 +1,4 @@
 // filename: Shape.cpp
-// your name (first and last): Alejandro Barajas
-// your Chico Portal/INGInious username: Ajbarajas1
 // Short summary of the file contents: This contains the constructor of shape that initializes m_x and m_y
 #include "shape.h"
 #include <iostream>
